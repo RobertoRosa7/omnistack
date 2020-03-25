cb245f31747de37df5e85abfb7149053062e60ea
Roberto Rosa <roberto.rosa7@gmail.com>
2020-03-25 10:14:48 -0300

    first page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import './global.css';
 import Header from './Header';
+import Logon from './pages/logon';
 
 /**
  * JSX: integração do HTML dentro do JavaScript
@@ -7,21 +9,19 @@ import Header from './Header';
 function App() {
   // let counter = 0; sem estado reativo
   // let counter = useState(0); // com estado reativo, mas com imutabilidade
-  
   // useState return Array = [valor, funcaoDeAtualizacaoDoValor]
-  const [counter, setCounter] = useState(0);
-
-  
-  function increment(){
-    // counter++; // error há imutabilidade devido uso do useState
-    setCounter(counter + 1); // uso da função para atualização do valor
-  }
+  // const [counter, setCounter] = useState(0);
+  // function increment(){
+  //   // counter++; // error há imutabilidade devido uso do useState
+  //   // setCounter(counter + 1); // uso da função para atualização do valor
+  // }
 
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    // <div>
+    //   <Header>Contador: {counter}</Header>
+    //   <button onClick={increment}>Incrementar</button>
+    // </div>
+    <Logon />
   );
 }
 
---
+++ frontend/src/pages/logon/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+import { FiLogIn } from 'react-icons/fi';
+import herosImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
+export default function Logon(){
+    return (
+        <section className="logon">
+            <div className="logon-container">
+                <div className="form">
+                    <img src={logoImg} alt="Be the Hero"/>
+                    <form>
+                        <h1>Faça seu logon</h1>
+                        <input placeholder="Sua ID"/>
+                        <button className="button" type="submit">Entrar</button>
+                        <a href="/register">
+                            <FiLogIn size={16} color="#e02041"/>
+                            Não tenho cadastro
+                        </a>
+                    </form>
+                </div>
+                <img src={herosImg} alt="heros"/>
+            </div>
+        </section>
+    );
+}
