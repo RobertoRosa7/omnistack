8f9aa2674eb51672c0c7f3b067838a8405dc0808
Roberto <roberto.rosa7@gmail.com>
2020-06-14 10:14:48 -0300

    format code to clear visualization and simplify too

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -5,7 +5,7 @@ import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
-export default function Profile(){
+export default function Profile() {
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
     const [incidents, setIncidents] = useState([]);
@@ -16,11 +16,11 @@ export default function Profile(){
             .then(response => setIncidents(response.data))
             .catch((e) => console.error(e));
     }, [ongId])
-    
+
     async function handleDeleteIncident(id){
         try{
             await api.delete(`incidents/${id}`, {headers:{Authorization:ongId}});
-            setIncidents(incidents.filter(incident => incident.id != id));
+            setIncidents(incidents.filter(incident => incident.id !== id));
         }catch(e){
             console.error(e);
         }
@@ -57,44 +57,8 @@ export default function Profile(){
                             <button onClick={() => handleDeleteIncident(incident.id)} type="button" ><FiTrash2 size={20} color="#a8a8b3"/></button>
                         </li>
                     ))}
-                    {/* <li>
-                        <strong>CASO:</strong>
-                        <p>caso teste</p>
-
-                        <strong>DESCRIÇÃO:</strong>
-                        <p>descrição teste</p>
-
-                        <strong>VALOR:</strong>
-                        <p>R$ 1.200,00</p>
-
-                        <button type="button" ><FiTrash2 size={20} color="#a8a8b3"/></button>
-                    </li>
-                    <li>
-                        <strong>CASO:</strong>
-                        <p>caso teste</p>
-
-                        <strong>DESCRIÇÃO:</strong>
-                        <p>descrição teste</p>
-
-                        <strong>VALOR:</strong>
-                        <p>R$ 1.200,00</p>
-
-                        <button type="button" ><FiTrash2 size={20} color="#a8a8b3"/></button>
-                    </li>
-                    <li>
-                        <strong>CASO:</strong>
-                        <p>caso teste</p>
-
-                        <strong>DESCRIÇÃO:</strong>
-                        <p>descrição teste</p>
-
-                        <strong>VALOR:</strong>
-                        <p>R$ 1.200,00</p>
-
-                        <button type="button" ><FiTrash2 size={20} color="#a8a8b3"/></button>
-                    </li> */}
                 </ul>
             </div>
         </section>
-    );
+    )
 }
